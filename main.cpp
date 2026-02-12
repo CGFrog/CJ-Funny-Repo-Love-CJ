2c814376cc26ea5ba17e829e264203964950a83c
Ender <Castornkasey@gmail.com>
2026-02-11 19:03:28 -0600

    Point Slope Formula
    Made a function to calculate the slope between two points!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
+#include <ostream>
+using namespace std;
+
 #include "point.cpp"
 int main() {
     
-    point p = point(1, 3);
-    point p2 = point(1, 3);
+    point p = point(2, 19);
+    point p2 = point(2, 19);
     point num = p + p2;
-
+    cout << num.x << ", " << num.y << endl;
+    float slope = p.slope(p,p2);
+    cout << slope << endl;
     return 0;
 }
--- point.cpp
+++ point.cpp
@@ -1,9 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 struct point{
     float x;
     float y;
     point(float x, float y){
         this->x=x;
         this->y=y;
+    }
+
+    float slope(point a, point b) {
+        if ((b.x - a.x) == 0) {
+            cout << "Divide by Zero Error!";
+            return 0;
+        }
+        else{
+            return (b.y-a.y)/(b.x-a.x);
+        }
     }
 
     bool operator==(point a) const {
